fb34f3fbe7638304d492a8712c273ca3b1a559ac
Arthur Melkonyan <arthur@artmelkon.me>
2022-03-25 12:43:45 +0200

    update Untracked files

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Row } from "react-bootstrap";
+
+import ScoopOption from "./ScoopOption";
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // optionType is 'scoops' or 'toppings'
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((err) => {
+        // TODO: handle error response
+        console.log(err);
+      });
+  }, [optionType]);
+
+  // TODO: replace 'null' with ToppingOption when avialable
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
