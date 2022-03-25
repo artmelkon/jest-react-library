47e2dd02487ce892d5987c94ccbbdde83500ccd8
Arthur Melkonyan <arthur@artmelkon.me>
2022-03-25 12:42:07 +0200

    code and test query scoop options image from server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-
+import logo from "./logo.svg";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,9 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-// src/mocks/server.js
-import { setupServer } from 'msw/node'
-import { handlers } from './handlers'
+// src/setupTests.js
+import { server } from './mocks/server.js'
 
-// This configures a request mocking server with the given request handlers.
-export const server = setupServer(...handlers)
+// Establish API mocking before all tests.
+beforeAll(() => server.listen())
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers())
+
+// Clean up after the tests are finished.
+afterAll(() => server.close())
